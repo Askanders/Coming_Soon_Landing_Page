2cabe19529ea0aff2a35e6ffb6cb34fdd94b04f9
Askanders <bruni.alessandro.ita@gmail.com>
2020-03-22 12:35:20 +0100

    refactor

---
+++ validate.js
@@ -0,0 +1,29 @@
+function validate() {
+  const email = document.getElementById("email").value;
+  // Regex from w3resource -> URL: https://www.w3resource.com/javascript/form/email-validation.php
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    const errorImg = document.querySelector(".error-img");
+    const errorText = document.querySelector(".error-text");
+
+    const errorElements = [errorImg, errorText];
+
+    errorElements.forEach((element) => {
+      element.classList.remove("show");
+      element.classList.add("hide");
+    });
+
+    document.querySelector("form").classList.remove("error-form");
+  } else {
+    const errorImg = document.querySelector(".error-img");
+    const errorText = document.querySelector(".error-text");
+
+    const errorElements = [errorImg, errorText];
+
+    errorElements.forEach((element) => {
+      element.classList.remove("hide");
+      element.classList.add("show");
+    });
+
+    document.querySelector("form").classList.add("error-form");
+  }
+}
