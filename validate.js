363ccbc3984e02a1d3b03fa1bb067a8b7921c7a0
Askanders <bruni.alessandro.ita@gmail.com>
2020-03-24 11:17:44 +0100

    little refactor

--- validate.js
+++ validate.js
@@ -1,29 +1,41 @@
 function validate() {
   const email = document.getElementById("email").value;
-  // Regex from w3resource -> URL: https://www.w3resource.com/javascript/form/email-validation.php
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-    const errorImg = document.querySelector(".error-img");
-    const errorText = document.querySelector(".error-text");
 
-    const errorElements = [errorImg, errorText];
-
-    errorElements.forEach((element) => {
-      element.classList.remove("show");
-      element.classList.add("hide");
-    });
-
-    document.querySelector("form").classList.remove("error-form");
+  if (isValidEmail(email)) {
+    hideErrorMessages();
   } else {
-    const errorImg = document.querySelector(".error-img");
-    const errorText = document.querySelector(".error-text");
-
-    const errorElements = [errorImg, errorText];
-
-    errorElements.forEach((element) => {
-      element.classList.remove("hide");
-      element.classList.add("show");
-    });
-
-    document.querySelector("form").classList.add("error-form");
+    showErrorMessages();
   }
+}
+
+function isValidEmail(email) {
+  // Regex from w3resource -> URL: https://www.w3resource.com/javascript/form/email-validation.php
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+function hideErrorMessages() {
+  const errorImg = document.querySelector(".error-img");
+  const errorText = document.querySelector(".error-text");
+
+  const errorElements = [errorImg, errorText];
+
+  errorElements.forEach((element) => {
+    element.classList.remove("show");
+    element.classList.add("hide");
+  });
+
+  document.querySelector("form").classList.remove("error-form");
+}
+
+function showErrorMessages() {
+  const errorImg = document.querySelector(".error-img");
+  const errorText = document.querySelector(".error-text");
+
+  const errorElements = [errorImg, errorText];
+
+  errorElements.forEach((element) => {
+    element.classList.remove("hide");
+    element.classList.add("show");
+  });
+
+  document.querySelector("form").classList.add("error-form");
 }
